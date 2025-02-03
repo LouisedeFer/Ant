3302d7a99c4a6050aedcb9ddc23a037e671a4401
Louise de Ferran <louise.deferran@orange.fr>
2025-02-03 09:58:56 +0100

    Start of the game

---
+++ ant/observer.py
@@ -0,0 +1,10 @@
+# Standard
+import typing
+
+
+class Observer:
+    """Interface representing an observer for the Observer pattern."""
+
+    def __init__(self) -> None:
+        """Object initialization."""
+        super().__init__()
